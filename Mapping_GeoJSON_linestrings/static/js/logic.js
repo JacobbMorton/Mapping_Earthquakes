9dec896e3fdb43d50288c9a52e874156ad8e644a
AbsoluteMorty <85866612+AbsoluteMorty@users.noreply.github.com>
2021-12-01 17:46:11 -0600

    push

--- Mapping_GeoJSON_linestrings/static/js/logic.js
+++ Mapping_GeoJSON_linestrings/static/js/logic.js
@@ -1,7 +1,7 @@
 console.log("working");
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
@@ -22,7 +22,7 @@ let map = L.map('mapid', {
 })
 // Create a base layer that holds both maps.
 let baseMaps = {
-    Street: streets,
+    Light: streets,
     Dark: dark
 };
 
